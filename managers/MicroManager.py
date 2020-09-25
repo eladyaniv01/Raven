e3a25c63223b0c512c43f8047f5229b6ee1cf23e
eladyaniv01 <eladyaniv01@gmail.com>
2020-09-25 19:30:14 +0300

    TODO optimize issue commands to not spam -> single command should be sent per order

--- managers/MicroManager.py
+++ managers/MicroManager.py
@@ -4,5 +4,9 @@ from .ManagerBase import BaseManager
 
 
 class MicroManager(BaseManager):
+
     def __init__(self, bot: BotAI) -> None:
         super().__init__(bot)
+
+    async def update(self, iteration: int) -> None:
+        pass
--- managers/StrategyManager.py
+++ managers/StrategyManager.py
@@ -6,3 +6,6 @@ from .ManagerBase import BaseManager
 class StrategyManager(BaseManager):
     def __init__(self, bot: BotAI) -> None:
         super().__init__(bot)
+
+    async def update(self, iteration: int) -> None:
+        pass
